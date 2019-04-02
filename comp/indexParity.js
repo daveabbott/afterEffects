3d03093828f94dc61c8cae0d4be0373496e69855
Dave Abbott <dave.abbott@me.com>
2019-04-02 13:25:30 +1100

    Added indexParity

---
+++ comp/indexParity.js
@@ -0,0 +1,9 @@
+// Reports index parity of a layer.
+// Useful for adding limited variation to duplicate layers.
+// Result is one of two values: Odd = 1. Even = 0.
+
+index%2
+
+// Example of use.
+
+if (index%2 == 1) x = 10 else x = 20;
